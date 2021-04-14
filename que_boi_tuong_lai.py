71aa707fb0521644f3d931c42c024c8cd391b77f
lebaoloc1 <65651044+lebaoloc1@users.noreply.github.com>
2021-04-14 22:06:31 +0700

    Add files via upload

---
+++ que_boi_tuong_lai.py
@@ -0,0 +1,5 @@
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+
+print(f"\nYour name is {name.title()}.")
+print(f"\nAfter {100-age} years, you will be 100 years old!")
